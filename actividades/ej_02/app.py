57916f5966088b66b6da780f4efaaa4f6d8a2673
Francisco-Melchor <fmelchor2005@gmail.com>
2024-11-26 16:25:49 -0300

    feat: realizo la actividad 2

--- actividades/ej_02/app.py
+++ actividades/ej_02/app.py
@@ -1,1 +1,30 @@
-# Aplicacion del servidor
+app = Microdot()
+
+# Definimos la ruta raíz
+@app.route('/')
+async def index(request):
+    return send_file('index.html')
+
+
+
+
+@app.route('/<dir>/<file>')
+async def static(request, dir, file):
+    return send_file("/{}/{}".format(dir, file))
+
+
+@app.route('/toggle/led/<int:id>') # Definimos la ruta con un parámetro entero
+async def index(request, id):
+
+    # Dependiendo del valor del parámetro id, encendemos o apagamos un LED
+    if id == 1:
+        LED1.value(not LED1.value())
+
+    elif id == 2:
+        LED2.value(not LED2.value())
+
+    elif id == 3:
+        LED3.value(not LED3.value())
+
+    return 'OK'
+app.run(port=80)
--- actividades/ej_02/boot.py
+++ actividades/ej_02/boot.py
@@ -1,1 +1,20 @@
-# Configuracion inicial
+from microdot import Microdot, send_file
+from machine import Pin, PWM
+import time
+
+LED1 = Pin(32, Pin.OUT)
+LED2 = Pin(33, Pin.OUT)
+LED3 = Pin(25, Pin.OUT)
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('CONECTANDO')
+        sta_if.active(True)
+        sta_if.connect('Cooperadora Alumnos', '')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
